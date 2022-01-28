f0ccd81cf12789a719c30c2074749c8640fe1da0
bhambeck <bhambeck@gmail.com>
2022-01-28 15:11:08 -0300

    html

---
+++ modulo4/primerProyectoHTML/app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path')
+
+const publicPath = path.resolve(__dirname, ('./public'));
+app.use (express.static(publicPath))
+
+app.listen(3001, ()=> console.log('Servidor funcionando en puerto 3001'))
+
+app.get ('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+
+})
