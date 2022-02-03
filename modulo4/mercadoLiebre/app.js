11578d8e673376b1841a9f5d7d7ab488d9fd8bd8
bhambeck <bhambeck@gmail.com>
2022-02-03 17:00:10 -0300

    fin modulo4

--- modulo4/mercadoLiebre/app.js
+++ modulo4/mercadoLiebre/app.js
@@ -9,5 +9,10 @@ app.listen(3001, ()=> console.log('Servidor funcionando en puerto 3001'))
 
 app.get ('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
-
+})
+app.get('/registro', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
